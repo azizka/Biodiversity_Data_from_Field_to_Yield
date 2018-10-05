6f1c5e3e79efbf56fc5e9e2f90d077ecde8bbf85
Alexander Zizka <alexander.zizka@bioenv.gu.internal>
2018-10-05 12:19:26 +0200

    tutorial changes

--- docs/talks/00_demonstration.R
+++ docs/talks/00_demonstration.R
@@ -6,9 +6,11 @@ library(sp)
 library(countrycode)
 library(CoordinateCleaner)
 
-
+##########
+#########Exercise 1
+##########
 #Search occurrence records
-dat <- occ_search(scientificName = "Wittmackia patentissima", 
+dat <- occ_search(scientificName = "Aechmea patentissima", 
                   return =  "data")
 
 
@@ -22,7 +24,7 @@ tax_key <- name_suggest(q = "Magnoliopsida", rank = "Class")
 #Sometimes groups have multiple taxon keys, in this case three, so we will check how many records are available for them
 lapply(tax_key$key, "occ_count")
 
-occ_count(tax_key, country = "BR")
+occ_count(tax_key[[1]], country = "BR")
 
 # Define study area and download data
 study_a <- "POLYGON((-35 -5.5, -37 -5.5, -37 -6.5, -35 -6.5, -35 -5.5))"
@@ -31,14 +33,26 @@ study_a <- "POLYGON((-35 -5.5, -37 -5.5, -37 -6.5, -35 -6.5, -35 -5.5))"
 #dat_ne  <- occ_search(taxonKey = tax_key, return = "data", hasCoordinate = T,
 #                      geometry = study_a, limit = 50000) 
 
+#write_csv(dat_ne, path = "inst/gbif_occurrences.csv")
 
+
+##############
 ##2. Data cleaning
+#############
 
 # load gbif data
 dat <- read_csv("inst/gbif_occurrences.csv", guess_max = 25000)%>%
   mutate(dataset = "GBIF")
 
-# add the field data
+names(dat) #a lot of columns
+
+dat <- dat %>%
+  select(species, decimalLongitude, decimalLatitude, countryCode, individualCount,
+         gbifID, family, taxonRank, coordinateUncertaintyInMeters, year,
+         basisOfRecord, institutionCode, datasetName, dataset)%>% # you might find other ones useful depending on your downstream analyses
+  mutate(countryCode = countrycode(dat$countryCode, origin =  'iso2c', destination = 'iso3c'))
+
+#read the field data
 field <- read_csv("inst/field.csv")
 
 field <- field%>%
@@ -55,7 +69,6 @@ dat <- bind_rows(dat, field)
 
 
 #Visualize on a map
-
 world.inp  <- map_data("world")
 
 ggplot()+
@@ -67,10 +80,6 @@ ggplot()+
   coord_fixed()+
   theme_bw()+
   theme(axis.title = element_blank())
-
-# Visualize on another map
-
-
 
 # Cleaning metadata
 # remove records without coordinates
@@ -90,12 +99,6 @@ table(dat$basisOfRecord)
 dat_cl <- filter(dat_cl, basisOfRecord == "HUMAN_OBSERVATION" | basisOfRecord == "OBSERVATION" |
                    basisOfRecord == "PRESERVED_SPECIMEN" | is.na(basisOfRecord))
 
-dat_cl <- filter(dat_cl, basisOfRecord == "HUMAN_OBSERVATION" | basisOfRecord == "OBSERVATION" |
-                   basisOfRecord == "PRESERVED_SPECIMEN")
-
-
-
-
 #Individual count
 table(dat_cl$individualCount)
 
@@ -109,19 +112,13 @@ table(dat_cl$year)
 dat_cl <- dat_cl%>%
   filter(year > 1945) # remove records from before second world war
 
-
+# Check family
 table(dat_cl$family) #that looks good
-dat_cl <- dat_cl%>%
-  filter(family == 'Malvaceae')
+
 
 table(dat_cl$taxonRank) # We will only include records identified to species level
 dat_cl <- dat_cl%>%
-  filter(taxonRank == "SPECIES")
-
-
-#Automated cleaning
-#convert country code from ISO2c to ISO3c
-dat_cl$countryCode <-  countrycode(dat_cl$countryCode, origin =  'iso2c', destination = 'iso3c')
+  filter(taxonRank == "SPECIES" | is.na(taxonRank))
 
 #flag problems
 dat_cl <- data.frame(dat_cl)
@@ -131,14 +128,6 @@ flags <- clean_coordinates(x = dat_cl, lon = "decimalLongitude", lat = "decimalL
                            tests = c("capitals", "centroids", "equal","gbif", "institutions",
                                      "zeros", "countries", "seas"),
                            seas_ref = buffland) # most test are on by default
-
-
-
-summary(flags)
-plot(flags, lon = "decimalLongitude", lat = "decimalLatitude")
-
-dat_cl <- dat_cl[flags$.summary, ]
-
 
 
 #For the total dataset
@@ -151,18 +140,30 @@ clean_dataset(dat_cl,
               lat = "decimalLatitude")
 
 
+world.inp  <- map_data("world")
 
-wm <- borders("world", colour="gray50", fill="gray50")
-ggplot() +
-  coord_fixed()+
-  wm +
+ggplot()+
+  geom_map(data=world.inp, map=world.inp, aes(x=long, y=lat, map_id=region), fill = "grey80")+
+  xlim(min(dat$decimalLongitude, na.rm = T), max(dat$decimalLongitude, na.rm = T))+
+  ylim(min(dat$decimalLatitude, na.rm = T), max(dat$decimalLatitude, na.rm = T))+
   geom_point(data = dat, aes(x = decimalLongitude, y = decimalLatitude),
              colour = "darkred", size = 1)+
   geom_point(data = dat_cl, aes(x = decimalLongitude, y = decimalLatitude),
              colour = "darkgreen", size = 1)+
   coord_fixed()+
-  xlim(min(dat$decimalLongitude), max(dat$decimalLongitude))+
-  ylim(min(dat$decimalLatitude), max(dat$decimalLatitude))
+  theme_bw()+
+  theme(axis.title = element_blank())
+
+
+ggplot()+
+  geom_map(data=world.inp, map=world.inp, aes(x=long, y=lat, map_id=region), fill = "grey80")+
+  xlim(min(dat$decimalLongitude, na.rm = T), max(dat$decimalLongitude, na.rm = T))+
+  ylim(min(dat$decimalLatitude, na.rm = T), max(dat$decimalLatitude, na.rm = T))+
+  geom_point(data = dat_cl, aes(x = decimalLongitude, y = decimalLatitude, colour = dataset),
+             size = 1)+
+  coord_fixed()+
+  theme_bw()+
+  theme(axis.title = element_blank())
 
 
 write_csv(dat_cl, "inst/occurrence_records_clean.csv")
